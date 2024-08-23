d7318301a5218b02b9f5c5c00fcc654d10eaaedf
Lorenzo393 <loloc.gelos@outlook.com>
2024-08-23 10:23:55 -0300

    Agrego ej 3 P1 python

--- Programacion II - Pyton/Practica 1/Practica1.py
+++ Programacion II - Pyton/Practica 1/Practica1.py
@@ -6,9 +6,24 @@ def pares(n):
     else:
         print(2)
 
-pares(25)
-pares(100)
+#pares(25)
+#pares(100)
  # 1. && 2. Esta misma solucion sirve para cualquiera de los dos ejercicios y 
  # para cualquier n natural
 
- # Ej 3
+# Ej 3
+def paresm(n , m):
+    if n > 1:
+        if (m % 2) == 0:
+            m = m + 2
+        else:
+            m = m + 1
+        print(m)
+        paresm(n - 1 , m)
+    else:
+        print(m + 2)
+
+#paresm(5 , 5)
+ # En este caso no pude ya que estas dos funciones trabajan de distinta manera
+
+# Ej 4
