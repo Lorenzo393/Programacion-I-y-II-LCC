cc0a5b779717c54b0142964f227be9d2de8244a4
Lorenzo393 <loloc.gelos@outlook.com>
2024-10-16 15:13:20 -0300

    Avanzo en el Ta Te Ti

---
+++ Programacion II - Python/Juegos/Ta Te Ti.py
@@ -0,0 +1,13 @@
+from random import randrange
+# randrange(x)      -> Genera un numero aleatorio entero entre 0 y x-1
+
+
+
+def iniciar_tablero():
+    tablero = [["-","-","-"],["-","-","-"],["-","-","-"]]
+    return tablero
+
+def mostrar_tablero(tablero):
+    for i in tablero:
+        print(i)
+            
---
+++ Programacion II - Python/Juegos/TaTeTi.py
@@ -0,0 +1,7 @@
+def iniciar_tablero():
+    tablero = ["-"]*9
+    return tablero
+
+def main():
+    tablero = iniciar_tablero()
+    print(tablero)
