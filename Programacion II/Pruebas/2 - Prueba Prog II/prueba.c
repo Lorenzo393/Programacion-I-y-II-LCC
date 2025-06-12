79c38c4fabf73638893d46f1a3b607c23b6158af
Lorenzo393 <loloc.gelos@outlook.com>
2025-06-12 15:14:40 -0300

    Prueba 2: Termino programa de C

--- Programacion II/Pruebas/2 - Prueba Prog II/prueba.c
+++ Programacion II/Pruebas/2 - Prueba Prog II/prueba.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 #include <time.h>
 
-#define CANT_PERSONAS_BASE 10
-#define CANT_LOCALIDADES_BASE 10
+#define MEMORIA_BASE_STRUCT 10
 #define SIZE_BUFFER_BASE 100
 #define MULT_CAPACIDAD 5
 #define EDAD_MAXIMA 100
@@ -22,15 +21,25 @@ typedef struct _listaPersonas{
     int capacidad;
 } ListaPersonas;
 
+typedef struct _localidades{
+    char *nombre;
+}Localidad;
+
 typedef struct _listaLocalidades{
-    char *localidades;
+    Localidad *localidades;
     int cantLocalidades;
     int capacidad;
 }ListaLocalidades;
 
-void mostrar_ListaPersonas_limited(ListaPersonas listaPersonas){
+void mostrar_ListaPersonas(ListaPersonas listaPersonas){
     for(int i = 0 ; i < listaPersonas.cantPersonas ; i++){
-        printf("%s - %s - %c - %i - %s\n",listaPersonas.persona[i].nombre,listaPersonas.persona[i].apellido,listaPersonas.persona[i].genero,listaPersonas.persona[i].edad,listaPersonas.persona[i].generoInt);
+        printf("%s - %s - %c - %i - %s - %s\n",listaPersonas.persona[i].nombre,listaPersonas.persona[i].apellido,listaPersonas.persona[i].genero,listaPersonas.persona[i].edad,listaPersonas.persona[i].generoInt,listaPersonas.persona[i].localidad);
+    }
+}
+
+void mostrar_ListaLocalidades(ListaLocalidades listaLocalidades){
+    for(int i = 0 ; i < listaLocalidades.cantLocalidades ; i++){
+        printf("%s\n",listaLocalidades.localidades[i].nombre);
     }
 }
 
@@ -42,7 +51,7 @@ void quitar_terminador(char buffer[]){
     buffer[strlen(buffer)-1] = '\0';
 }
 
-char *cargar_cadena(int indiceBuffer, char buffer[]){
+char *cargar_cadena_coma(int indiceBuffer, char buffer[]){
     char aux[SIZE_BUFFER_BASE];
     int indiceAux;
     for(indiceAux = 0 ; buffer[indiceBuffer] != ',' ; indiceAux++, indiceBuffer++){
@@ -57,14 +66,23 @@ char *cargar_cadena(int indiceBuffer, char buffer[]){
     return cadena;
 }
 
+char *cargar_cadena_saltoLinea(char buffer[]){
+    quitar_terminador(buffer);
+    char *cadena = malloc(sizeof(char) * strlen(buffer) + 1);
+    strcpy(cadena,buffer);
+    cadena[strlen(cadena)] = '\0';
+
+    return cadena;
+}
+
 Persona cargar_Personas(char buffer[]){
     Persona persona;
     int indiceBuffer = 0;
 
-    persona.nombre = cargar_cadena(indiceBuffer,buffer);
+    persona.nombre = cargar_cadena_coma(indiceBuffer,buffer);
     indiceBuffer = strlen(persona.nombre) + 1;
 
-    persona.apellido = cargar_cadena(indiceBuffer,buffer);
+    persona.apellido = cargar_cadena_coma(indiceBuffer,buffer);
     indiceBuffer += strlen(persona.apellido) + 1;
 
     persona.genero = buffer[indiceBuffer];
@@ -87,21 +105,29 @@ ListaPersonas cargar_EntradaPersonas(FILE* filePersonas, ListaPersonas listaPers
             listaPersonas.cantPersonas++;
         }
     }
+    if(leer_linea(buffer,filePersonas) == NULL) printf("\nEOF\n");
     return listaPersonas;
 }
-/*
-ListaPersonas cargar_EntradaLocalidades(FILE *fileLocalidades, ListaPersonas listaPersonas){
-    char buffer[SIZE_BUFFER_BASE];
-    //char **localidades = malloc((char **)  CANT_LOCALIDADES_BASE);
 
+ListaLocalidades cargar_EntradaLocalidades(FILE *fileLocalidades, ListaLocalidades listaLocalidades){
+    char buffer[SIZE_BUFFER_BASE];
     while(leer_linea(buffer,fileLocalidades) != NULL){
-        quitar_terminador(buffer);
-        printf("%s",buffer);
+        if(listaLocalidades.cantLocalidades < listaLocalidades.capacidad){
+            listaLocalidades.localidades[listaLocalidades.cantLocalidades].nombre = cargar_cadena_saltoLinea(buffer);
+            listaLocalidades.cantLocalidades++;
+        }
+        else{
+            int nuevaCapacidad = listaLocalidades.capacidad * MULT_CAPACIDAD;
+            listaLocalidades.localidades = realloc(listaLocalidades.localidades,(sizeof(Localidad) * nuevaCapacidad));
+            listaLocalidades.capacidad = nuevaCapacidad;
+            listaLocalidades.localidades[listaLocalidades.cantLocalidades].nombre = cargar_cadena_saltoLinea(buffer);
+            listaLocalidades.cantLocalidades++;
+        }
     }
-    
-    return listaPersonas;
+    if(leer_linea(buffer,fileLocalidades) == NULL) printf("\nEOF\n");
+    return listaLocalidades;
 }
-*/
+
 char *copiar_cadena(char cadena[]){  
     char *generoInt = malloc(sizeof(char) * strlen(cadena)+1);
     strcpy(generoInt,cadena);
@@ -129,21 +155,60 @@ char *asignar_generoInt(){
     return generoInt;
 }
 
-ListaPersonas cargar_datos_aleatorios(ListaPersonas listaPersonas){
+char *asignar_Localidad(ListaLocalidades listaLocalidades){
+    int nLocalidad = rand() % listaLocalidades.cantLocalidades;
+    char *cadena = malloc(sizeof(char) * (strlen(listaLocalidades.localidades[nLocalidad].nombre)+1));
+    strcpy(cadena,listaLocalidades.localidades[nLocalidad].nombre);
+    cadena[strlen(listaLocalidades.localidades[nLocalidad].nombre)] = '\0';
+    return cadena;
+}
+
+void imprimir_datos_archivo(FILE *file, ListaPersonas listaPersonas){
+    for(int i = 0 ; i < listaPersonas.cantPersonas ; i++){
+        fprintf(file,"%s,%s,%c,%i,%s,%s\n",listaPersonas.persona[i].nombre,listaPersonas.persona[i].apellido,listaPersonas.persona[i].genero,listaPersonas.persona[i].edad,listaPersonas.persona[i].generoInt,listaPersonas.persona[i].localidad);
+    }
+}
+
+ListaPersonas cargar_datos_aleatorios(ListaPersonas listaPersonas, ListaLocalidades listaLocalidades){
     for(int i = 0 ; i < listaPersonas.cantPersonas ; i++){
         listaPersonas.persona[i].edad = rand() % EDAD_MAXIMA;
         listaPersonas.persona[i].generoInt = asignar_generoInt();
+        listaPersonas.persona[i].localidad = asignar_Localidad(listaLocalidades);
     }
-
     return listaPersonas;
+}
+
+void free_ListaLocalidades(ListaLocalidades *listaLocalidades){
+    for(int i = 0 ; i < listaLocalidades->cantLocalidades ; i++){
+        free(listaLocalidades->localidades[i].nombre);
+    }
+    listaLocalidades->cantLocalidades = 0;
+}
+
+void free_ListaPersonas(ListaPersonas *listaPersonas){
+    for(int i = 0 ; i < listaPersonas->cantPersonas ; i++){
+        free(listaPersonas->persona[i].nombre);
+        free(listaPersonas->persona[i].apellido);
+        free(listaPersonas->persona[i].generoInt);
+        free(listaPersonas->persona[i].localidad);
+    }
+    listaPersonas->cantPersonas = 0;
 }
 
 ListaPersonas inicializar_ListaPersonas(){
     ListaPersonas listaPersonas;
-    listaPersonas.persona = malloc(sizeof(Persona) * CANT_PERSONAS_BASE);
-    listaPersonas.capacidad = CANT_PERSONAS_BASE;
+    listaPersonas.persona = malloc(sizeof(Persona) * MEMORIA_BASE_STRUCT);
+    listaPersonas.capacidad = MEMORIA_BASE_STRUCT;
     listaPersonas.cantPersonas = 0;
     return listaPersonas;
+}
+
+ListaLocalidades inicializar_ListaLocalidades(){
+    ListaLocalidades listaLocalidades;
+    listaLocalidades.localidades = malloc(sizeof(Localidad) * MEMORIA_BASE_STRUCT);
+    listaLocalidades.capacidad = MEMORIA_BASE_STRUCT;
+    listaLocalidades.cantLocalidades = 0;
+    return listaLocalidades;
 }
 
 int main(){
@@ -158,20 +223,27 @@ int main(){
     listaPersonas = cargar_EntradaPersonas(filePersonas,listaPersonas);
     fclose(filePersonas);
 
-    /*
+    
     FILE *fileLocalidades = fopen("entrada_localidades.txt","r");
     if(fileLocalidades == NULL){
-        printf("Error al abrir \"entrada_personas.txt\" "); 
+        printf("Error al abrir \"entrada_localidades.txt\" "); 
         return 1;
     }
-    listaPersonas = cargar_EntradaLocalidades(fileLocalidades,listaPersonas);
+    ListaLocalidades listaLocalidades = inicializar_ListaLocalidades();
+    listaLocalidades = cargar_EntradaLocalidades(fileLocalidades,listaLocalidades);
     fclose(fileLocalidades);
-    */
+    
+    listaPersonas = cargar_datos_aleatorios(listaPersonas,listaLocalidades);
+    free_ListaLocalidades(&listaLocalidades);
+    
+    FILE *fileSalida = fopen("salida.txt","w");
+    if(fileSalida == NULL){
+        printf("Error al escribir en archivo \"salida.txt\" ");
+        return 1;
+    }
+    imprimir_datos_archivo(fileSalida,listaPersonas);
+    free_ListaPersonas(&listaPersonas);
+    fclose(fileSalida);
 
-    listaPersonas = cargar_datos_aleatorios(listaPersonas);
-
-    mostrar_ListaPersonas_limited(listaPersonas);
-
-    //imprimir_datos_archivo(fileDatosPersonas,listaPersonas);
     return 0;
 }
