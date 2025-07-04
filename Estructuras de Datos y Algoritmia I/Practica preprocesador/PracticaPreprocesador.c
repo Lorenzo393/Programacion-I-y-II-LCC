298d6b79c6c07edda5e7fd5659e6a7032eb9e9d3
Lorenzo393 <loloc.gelos@outlook.com>
2025-07-04 11:42:20 -0300

    Agrego material de estudio

---
+++ Estructuras de Datos y Algoritmia I/Practica preprocesador/PracticaPreprocesador.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define INICIO {
+#define FIN }
+#define porsiempre for(;;)
+#define paste(front,back) front ## back
+#define hola 1
+#define DIV(x,y) x/y
+
+int main()INICIO
+
+    float x = 1, y = 2;
+    printf("%f",DIV((x+y),(y+y)));
+
+
+    return 0;
+FIN
