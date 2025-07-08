1c82f85091d14597b8ac30580e1dc7849c3ac7a7
Lorenzo393 <loloc.gelos@outlook.com>
2025-07-08 13:49:48 -0300

    Practica 1: Completo funciones de matriz_uni

--- AD_Estructuras de Datos y Algoritmia I/Practica 1/Practica1.c
+++ AD_Estructuras de Datos y Algoritmia I/Practica 1/Practica1.c
@@ -1,12 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
+#include "matriz.h"
 
 //EJ 1
-int mediana([1.0,2.0,3.0,4.0,5.0]){
-   
-}
+
 
 
 int main(){
--- AD_Estructuras de Datos y Algoritmia I/Practica 1/matriz/matriz_uni.c
+++ AD_Estructuras de Datos y Algoritmia I/Practica 1/matriz/matriz_uni.c
@@ -5,28 +5,41 @@
 */
 
 struct Matriz_ {
+    int nFilas,nColumnas;
+    double **elementos;
 };
 
 
 Matriz* matriz_crear(size_t numFilas, size_t numColumnas) {
+    Matriz *matriz = malloc(sizeof(Matriz));
+    matriz->nFilas = numFilas;
+    matriz->nColumnas = numColumnas;
+    matriz->elementos = malloc(sizeof(double*)*numFilas);
+    for(int i = 0 ; i < numFilas ; i++){
+        matriz->elementos[i] = malloc(sizeof(double) * numColumnas);
+    }
+    return matriz;
 }
 
 void matriz_destruir(Matriz* matriz) {
-
+    for(int i = 0 ; i < matriz->nFilas ; i++){
+        free(matriz->elementos[i]);
+    }
+    free(matriz->elementos);
 }
 
 double matriz_leer(Matriz* matriz, size_t fil, size_t col) {
-
+    return matriz->elementos[fil][col];
 }
 
 void matriz_escribir(Matriz* matriz, size_t fil, size_t col, double val) {
-
+    matriz->elementos[fil][col] = val;
 }
 
 size_t matriz_num_filas(Matriz* matriz) {
-
+    return matriz->nFilas;
 }
 
 size_t matriz_num_columnas(Matriz* matriz) {
-
+    return matriz->nColumnas;
 }
